7e07f7c20403396778bf9dd548a60894f5a5491a
Marcus <marcuskristensen10@hotmail.com>
2025-11-25 10:24:55 +0100

    Name change

---
+++ Interfaces/IUserPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using LystfiskerPortalen.Models;
+
+namespace LystfiskerPortalen.Interfaces
+{
+    public interface IUserPostRepository
+    {
+        Task AddAsync(UserPost post);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(int id, UserPost newPost);
+        Task<List<UserPost>> GetAllAsync();
+        Task<UserPost> GetByIdAsync(int id);
+        Task<List<UserPost>> GetAllByUserIdAsync(string userId);
+    }
+}
