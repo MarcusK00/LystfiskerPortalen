cfcf33c43a964ee79fd2f428dbacd630a17725af
NoahBingBong <hilt1@outlook.com>
2025-12-02 11:41:27 +0100

    noahs post virker ikke

--- Models/UserPost.cs
+++ Models/UserPost.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string ImgSrc { get; set; }
+       
         public string Description { get; set; }
         public ApplicationUser User { get; set; }
         public Catch CatchInfo { get; set; }
