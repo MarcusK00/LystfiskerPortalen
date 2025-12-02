9c87d12abae079769225600cdeafe1f5b93f8999
Marcus <marcuskristensen10@hotmail.com>
2025-12-02 14:19:51 +0100

    changed interace to match model.

--- Interfaces/IUserPostHttpService.cs
+++ Interfaces/IUserPostHttpService.cs
@@ -4,7 +4,7 @@ namespace LystfiskerPortalen.Interfaces
     public interface IUserPostHttpService
     {
         public Task AddAsync(UserPost userPost);
-        public Task DeleteAsync(UserPost userPost);
+        public Task DeleteAsync(int id);
         public Task<UserPost> UpdateAsync(UserPost userPost);
         public Task<List<UserPost>> GetAllAsync();
         public Task<UserPost> GetByIdAsync(int id);
