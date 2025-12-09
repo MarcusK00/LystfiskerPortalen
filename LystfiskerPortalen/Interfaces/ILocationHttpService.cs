1d9a22b0fc1843dc1197e58e3e9f1b63d031af97
Marcus <marcuskristensen10@hotmail.com>
2025-12-09 12:20:09 +0100

    Lavede controller, service og interface for location repo.

---
+++ LystfiskerPortalen/Interfaces/ILocationHttpService.cs
@@ -0,0 +1,10 @@
+﻿using LystfiskerPortalen.Models;
+
+namespace LystfiskerPortalen.Interfaces
+{
+    public interface ILocationHttpService
+    {
+        public Task<List<Location>> GetAllAsync();
+        public Task<Location> GetByIdAsync(int id);
+    }
+}
