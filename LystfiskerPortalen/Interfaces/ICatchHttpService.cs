6600d6b17659e6baf06bd66ebe626eb66ca44079
Jonathan L. <70529549+Hairo03@users.noreply.github.com>
2025-12-12 12:03:45 +0100

    Added CatchHttpService

---
+++ LystfiskerPortalen/Interfaces/ICatchHttpService.cs
@@ -0,0 +1,13 @@
+﻿using LystfiskerPortalenShared.Models;
+
+namespace LystfiskerPortalen.Interfaces
+{
+    public interface ICatchHttpService
+    {
+        public Task AddAsync(Catch userCatch);
+        public Task DeleteAsync(int id);
+        public Task<Catch> UpdateAsync(Catch userCatch);
+        public Task<List<Catch>> GetAllAsync();
+        public Task<Catch> GetByIdAsync(int id);
+    }
+}
---
+++ LystfiskerPortalen/Services/CatchHttpService.cs
@@ -0,0 +1,33 @@
+﻿using LystfiskerPortalen.Interfaces;
+using LystfiskerPortalenShared.Models;
+
+namespace LystfiskerPortalen.Services
+{
+    public class CatchHttpService : ICatchHttpService
+    {
+        public Task AddAsync(Catch userCatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Catch>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Catch> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Catch> UpdateAsync(Catch userCatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
