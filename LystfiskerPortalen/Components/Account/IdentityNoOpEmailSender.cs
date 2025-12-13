b7ffed969652b1c2efd4d7bd8649bc67b4c599ad
Lindorf69 <rasmus.lindorf.h@gmail.com>
2025-12-13 18:34:20 +0100

    Identity

--- LystfiskerPortalen/Components/Account/IdentityNoOpEmailSender.cs
+++ LystfiskerPortalen/Components/Account/IdentityNoOpEmailSender.cs
@@ -1,21 +1,23 @@
-﻿using LystfiskerPortalenShared.Data;
+﻿using LystfiskerPortalenShared.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace LystfiskerPortalen.Components.Account
 {
-    // Remove the "else if (EmailSender is IdentityNoOpEmailSender)" block from RegisterConfirmation.razor after updating with a real implementation.
-    internal sealed class IdentityNoOpEmailSender : IEmailSender<IdentityUser>
+    // Vi ændrer interfacet her til <ApplicationUser> i stedet for <IdentityUser>
+    public class IdentityNoOpEmailSender : IEmailSender<ApplicationUser>
     {
-        private readonly IEmailSender emailSender = new NoOpEmailSender();
+        private readonly IEmailSender _emailSender = new NoOpEmailSender();
 
-        public Task SendConfirmationLinkAsync(IdentityUser user, string email, string confirmationLink) =>
-            emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+        // Herunder ændrer vi "IdentityUser user" til "ApplicationUser user" i alle metoderne
 
-        public Task SendPasswordResetLinkAsync(IdentityUser user, string email, string resetLink) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink) =>
+            _emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
 
-        public Task SendPasswordResetCodeAsync(IdentityUser user, string email, string resetCode) =>
-            emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink) =>
+            _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode) =>
+            _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password using the following code: {resetCode}");
     }
 }
--- LystfiskerPortalen/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
+++ LystfiskerPortalen/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
@@ -1,4 +1,4 @@
-﻿using LystfiskerPortalenShared.Data;
+﻿using LystfiskerPortalenShared.Models;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Identity;
@@ -7,8 +7,6 @@ using System.Security.Claims;
 
 namespace LystfiskerPortalen.Components.Account
 {
-    // This is a server-side AuthenticationStateProvider that revalidates the security stamp for the connected user
-    // every 30 minutes an interactive circuit is connected.
     internal sealed class IdentityRevalidatingAuthenticationStateProvider(
             ILoggerFactory loggerFactory,
             IServiceScopeFactory scopeFactory,
@@ -22,11 +20,11 @@ namespace LystfiskerPortalen.Components.Account
         {
             // Get the user manager from a new scope to ensure it fetches fresh data
             await using var scope = scopeFactory.CreateAsyncScope();
-            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
             return await ValidateSecurityStampAsync(userManager, authenticationState.User);
         }
 
-        private async Task<bool> ValidateSecurityStampAsync(UserManager<IdentityUser> userManager, ClaimsPrincipal principal)
+        private async Task<bool> ValidateSecurityStampAsync(UserManager<ApplicationUser> userManager, ClaimsPrincipal principal)
         {
             var user = await userManager.GetUserAsync(principal);
             if (user is null)
